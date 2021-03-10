b380e44d459bab15d0c57633c582ee6238a07446
da-moon <damoon.azarpazhooh@live.com>
2021-03-10 19:58:27 +0000

    faulty minikube

--- tools/fixtures/minikube/docker-entrypoint.sh
+++ tools/fixtures/minikube/docker-entrypoint.sh
@@ -1,4 +1,128 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
+# -*- mode: sh -*-
+# vi: set ft=sh:tabstop=2:softtabstop=2:shiftwidth=2:expandtab
+set -o errtrace
+set -o functrace
+set -o errexit
+set -o nounset
+set -o pipefail
+export SLEEP_DURATION=0
+COLUMNS=100
+stty columns ${COLUMNS}
+
+
+#
+# ─── FUNCTION SECTION ───────────────────────────────────────────────────────────
+#
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+if [ -n "$(command -v apt-get)" ]; then
+  export DEBIAN_FRONTEND=noninteractive
+  echo >&2 "*** Detected Debian based Linux"
+fi
+if ! command -v "tput" >/dev/null ; then
+  echo >&2  "*** 'tput' was not found in PATH"
+  exit 1
+fi
+# [ NOTE ] => Bold
+bold=$(tput bold)
+# [ NOTE ] => Red Color
+# red
+red=$(tput setaf 1)
+# [ NOTE ] => Green Color
+green=$(tput setaf 2)
+# [ NOTE ] => Yellow Color
+yellow=$(tput setaf 3)
+# [ NOTE ] => dark blue Color
+dblue=$(tput setaf 4)
+# [ NOTE ] => Reset color
+reset=$(tput sgr0)
+# printf-wrap
+# Description:  Printf with smart word wrapping for every columns.
+# Usage:        print-wrap "<text>" "<text>" ...
+function printf-wrap () {
+  local -r collen=$(($(tput cols)));
+  local keyname="$1";
+  local value=$2;
+  while [ -n "$value" ] ; do
+    printf >&2 "%-10s %-0s\n" "${keyname}" "${value:0:$collen}";
+    keyname="";
+    value=${value:$collen};
+  done
+}
+
+function err() {
+    local -r value=$1;
+    local -r keyname="${bold}[   ${red}ERROR${reset}${bold}  ]${reset}"
+    printf-wrap "${keyname}" "${value}"
+}
+function info() {
+    local -r value=$1;
+    local -r keyname="${bold}[   ${green}INFO${reset}${bold}   ]${reset}"
+    printf-wrap "${keyname}" "${value}"
+}
+
+function warn() {
+    local -r value=$1;
+    local -r keyname="${bold}[  ${yellow}WARNING${reset}${bold} ]${reset}"
+    printf-wrap "${keyname}" "${value}"
+}
+
+function section(){
+  if [[ $# == 0 ]]; then
+    err "this function needs arguments"
+    exit 1
+  fi
+  local -r terminal_cols="$(tput cols)"
+  while [[ $# -gt 0 ]]; do
+    local key="$1"
+    case "$key" in
+      --message)
+        local -r message="$2"
+        echo >&2 ""
+        info "${message}"
+        echo >&2 ""
+        shift
+      ;;
+      --params)
+        local key="$2"
+        shift
+        local value="$2"
+        shift
+        key="[   ${bold}${key}${reset}   ] =>"
+        value="${dblue}${value}${reset}"
+        printf-wrap "${key}" "${value}"
+      ;;
+      --command)
+        local command="$2"
+        echo >&2 "${yellow}"
+        printf "\n%${terminal_cols}s\n" | tr ' ' '-' >&2
+        echo >&2 ""
+        echo >&2 "  ${command}"
+        echo >&2 ""
+        printf "%${terminal_cols}s\n" | tr ' ' '-' >&2
+        echo >&2 "${reset}"
+        sleep "${SLEEP_DURATION}"
+        bash -c "${command}"
+        shift
+      ;;
+      *)
+        err "unacceptable arg ${red}${key}${reset}"
+        exit 1
+      ;;
+    esac
+    shift
+  done
+  printf "\n%${terminal_cols}s\n" | tr ' ' '#' >&2
+  sleep "${SLEEP_DURATION}"
+  echo >&2 ""
+}
+#
+# ──────────────────────────────────────────────────────────────────────────────────── I ──────────
+#   :::::: S C R I P T   E X E C U T I O N   S T A R T : :  :   :    :     :        :          :
+# ──────────────────────────────────────────────────────────────────────────────────────────────
+#
+
 
 #clean pid after unexpected kill
 if [ -f "/var/run/docker.pid" ]; then
@@ -8,19 +132,33 @@ fi
 # reread all config
 source /etc/profile
 
+echo >&2 ""
+
+message=$(cat <<EOF
+${red}
+─── KUBERNETES INIT ────────────────────────────────────────────────────────────
+${reset}
+EOF
+)
+
+echo >&2 "$message"
+
+# message="ensuring minikube has started"
+# command="minikube start || true"
+# section --message "$message" --command "$command"
+
 if [[ "$1" == 'minikube' ]]; then
-    echo "Starting minikube..."
+    echo >&2 "***Starting minikube..."
     minikube start \
-        --kubernetes-version=v${KUBERNETES_VERSION} \
         --extra-config=apiserver.Audit.LogOptions.Path="/var/log/apiserver/audit.log" \
         --extra-config=apiserver.Audit.LogOptions.MaxAge=30 \
         --extra-config=apiserver.Audit.LogOptions.MaxSize=100 \
         --extra-config=apiserver.Audit.LogOptions.MaxBackups=5 \
         --bootstrapper=localkube \
-        --vm-driver=none
+        --vm-driver=docker
 
     echo "Setting kubeconfig context..."
-    minikube update-context
+    sudo minikube update-context
 
     echo "Waiting for minkube to be ready..."
     # this for loop waits until kubectl can access the api server that Minikube has created
--- tools/minikube.Dockerfile
+++ tools/minikube.Dockerfile
@@ -2,26 +2,18 @@
 # eg. build command
 # docker build -t fjolsvin/consul-replicate --file consul-replicate.Dockerfile .
 FROM golang:alpine AS builder
-ARG KUBE_VERSION=1.7.5
-ARG MINIKUBE_VERSION=0.23.0
-ENV KUBERNETES_VERSION="${KUBE_VERSION}" 
 ENV LANG=en_US.UTF-8
-RUN apk add --no-cache git
-RUN go env -w GO111MODULE=off && \
-  go env -w "CGO_ENABLED=0" && \
-  go env -w "CGO_LDFLAGS=-s -w -extldflags '-static'"
-RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories && \
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories && \
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-    apk upgrade -U -a && \
+RUN go env -w GO111MODULE=on && \
+ go env -w "CGO_ENABLED=0" && \
+ go env -w "CGO_LDFLAGS=-s -w -extldflags '-static'"
+RUN apk upgrade -U -a && \
     apk add build-base make git bash ncurses upx \
     ca-certificates python2 wget  \
     autoconf findutils pkgconf libtool g++ automake linux-headers git
-
 WORKDIR "/go/src/k8s.io"
 RUN git clone \
       --depth 1 \
-      --branch "v${MINIKUBE_VERSION}" \
+      --branch "master" \
       "https://github.com/kubernetes/minikube.git" \
       "/go/src/k8s.io/minikube"
 WORKDIR "/go/src/k8s.io/minikube"
@@ -31,19 +23,19 @@ RUN make && \
     mv out/minikube /go/bin/minikube
 FROM alpine
 COPY --from=builder /go/bin/minikube /usr/local/bin/minikube
-COPY tools/fixtures/minikube/docker-entrypoint.sh /usr/local/bin/
 ARG USER=operator
 ENV USER $USER
 USER root
 RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" > /etc/apk/repositories && \
     echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories && \
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories && \
-    apk upgrade --no-cache -U -a && \
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+RUN apk upgrade --no-cache -U -a && \
     apk add --no-cache curl coreutils \
-    findutils wget jq git ncurses bash sudo shadow
+    findutils wget jq git ncurses bash sudo \
+    shadow openssl conntrack-tools docker kubeadm
 SHELL ["bash", "-c"]
 
-WORKDIR /home/"${USER}"
+WORKDIR "/home/${USER}"
 RUN getent group "${USER}" > /dev/null || addgroup "${USER}" > /dev/null 2>&1 || true
 RUN getent passwd "${USER}" > /dev/null || adduser \
   -G wheel \
@@ -56,7 +48,12 @@ RUN getent passwd "${USER}" > /dev/null || adduser \
   sed -i -e 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g' \
       /etc/sudoers && \
   chown "${USER}":"${USER}" /home/"${USER}" -R && \
-  usermod -aG wheel "${USER}"
+  usermod -aG wheel,docker "${USER}"
+RUN wget \
+  -qO /usr/local/bin/kubectl \
+  "https://storage.googleapis.com/kubernetes-release/release/$(curl -fSsL https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" && \
+  sudo chmod +x /usr/local/bin/kubectl
+RUN curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 USER "${USER}"
 ENV HOME /home/"${USER}"
 ENV MINIKUBE_WANTUPDATENOTIFICATION="false"
@@ -64,20 +61,20 @@ ENV MINIKUBE_WANTREPORTERRORPROMPT="false"
 ENV MINIKUBE_HOME="$HOME" 
 ENV CHANGE_MINIKUBE_NONE_USER="true"
 WORKDIR "/workspace"
-RUN sudo wget \
-  -O "/usr/local/bin/kubectl" \
-  "https://storage.googleapis.com/kubernetes-release/release/v${KUBERNETES_VERSION}/bin/linux/amd64/kubectl"  && \
-  sudo chmod +x /usr/local/bin/kubectl
-RUN sudo chmod +x /usr/local/bin/docker-entrypoint.sh
 
 # [NOTE] => confirming successful installation
 RUN { echo && echo "PS1='\[\e]0;\u \w\a\]\[\033[01;32m\]\u\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\] \\\$ '";} >> ~/.bashrc && \
-    kubectl --version
-RUN minikube start --vm-driver none --kubernetes-version v${KUBERNETES_VERSION} --memory 1024 --disk-size 4g 
-RUN sudo rc-update add docker boot && \
-    echo "export DOCKER_HOST='tcp://127.0.0.1:2375'" | sudo tee -a "/etc/profile" > /dev/null
+    kubectl version --client=true && \
+    minikube version && \
+    helm version
+RUN echo 'if [ -e /var/run/docker.sock ]; then sudo chown "$(id -u):docker" /var/run/docker.sock; fi' >> "/${HOME}/.bashrc"
+
+# RUN sudo service docker stop || true
+# RUN minikube start --vm-driver docker
 VOLUME ${HOME}/.minikube
-EXPOSE 2375 8443
-ENTRYPOINT ["docker-entrypoint.sh"]
-CMD ["minikube"]
+EXPOSE 8443
+COPY fixtures/minikube/docker-entrypoint.sh /usr/local/bin/
+RUN sudo chmod +x /usr/local/bin/docker-entrypoint.sh
+# ENTRYPOINT ["docker-entrypoint.sh"]
+# CMD ["minikube"]
 
