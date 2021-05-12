cf2248c0cb99024476e23e68b9e69f6815a78b06
da-moon <damoon.azarpazhooh@live.com>
2021-05-12 10:51:26 -0400

    fixed naming

--- devcontainer/rust/build.ps1
+++ devcontainer/rust/build.ps1
@@ -3,6 +3,7 @@ $IMAGE_NAME = "fjolsvin/rust-base-debian"
 $BUILDER = $IMAGE_NAME.Split('/')[-1]
 docker buildx use "${BUILDER}" 2>&1 | Out-Null ; if (-not $?) {
   docker buildx create --use --name "${BUILDER}"  2>&1 | Out-Null
+}
 $BUILD = 'docker buildx build'
 $BUILD += ' -f base\debian.Dockerfile'
 $BUILD += ' --cache-from type=registry,ref={0}:cache' -f $IMAGE_NAME
