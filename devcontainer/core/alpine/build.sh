f643cb023f53b7421ca58b5222080b78e59105c8
da-moon <damoon.azarpazhooh@live.com>
2021-06-23 19:08:07 +0000

    ci(devcontainer/core): devcontainer-core workflow and some minor fixes

--- devcontainer/core/alpine/build.sh
+++ devcontainer/core/alpine/build.sh
@@ -5,14 +5,10 @@ IMAGE_NAME="fjolsvin/base-alpine"
 WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
 # --------------------------------------------------------------------------------
-# export DOCKER_BUILDKIT=0
-# docker build -t $IMAGE_NAME .
 export DOCKER_BUILDKIT=1
 docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
 BUILD="docker buildx build"
 BUILD+=" --platform linux/amd64,linux/arm64"
-# BUILD+=" --platform linux/arm64"
-# BUILD+=" --platform linux/amd64"
 BUILD+=" --cache-from type=registry,ref=${IMAGE_NAME}:cache"
 BUILD+=" --cache-to type=registry,mode=max,ref=${IMAGE_NAME}:cache"
 BUILD+=" --tag ${IMAGE_NAME}:latest"
