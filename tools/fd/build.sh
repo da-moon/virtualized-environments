c8418db65bb0f9b079bd6637d9ff329e7873ef6d
da-moon <damoon.azarpazhooh@ryerson.ca>
2021-06-29 20:26:49 -0400

    feat(tools/fd): added dockerfile and buildscript

---
+++ tools/fd/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+IMAGE_NAME="fjolsvin/fd"
+CACHE_NAME="fjolsvin/cache:fd"
+WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
+export DOCKER_BUILDKIT=1
+docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
+BUILD="docker buildx build"
+BUILD+=" --platform linux/amd64,linux/arm64"
+BUILD+=" --cache-from type=registry,ref=${CACHE_NAME}"
+BUILD+=" --cache-to type=registry,mode=max,ref=${CACHE_NAME}"
+BUILD+=" --tag ${IMAGE_NAME}:latest"
+BUILD+=" --progress=plain"
+BUILD+=" --push"
+$BUILD $WD
+docker buildx use default
