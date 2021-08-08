9532f1ad28743940317a195b20fd16e59abc7c7a
da-moon <damoon.azarpazhooh@live.com>
2021-08-08 00:11:53 +0000

    refactor(builder/rust/alpine): better , more flexible build script

--- builder/rust/alpine/build.sh
+++ builder/rust/alpine/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #-*-mode:sh;indent-tabs-mode:nil;tab-width:2;coding:utf-8-*-
 # vi: tabstop=2 shiftwidth=2 softtabstop=2 expandtab:
-set -euxo pipefail ;
-if [ -z ${IMAGE_NAME+x} ] || [  -z ${IMAGE_NAME} ];then
+set -euxo pipefail
+if [ -z ${IMAGE_NAME+x} ] || [ -z ${IMAGE_NAME} ]; then
   IMAGE_NAME="fjolsvin/rust-builder-alpine"
 fi
 # ────────────────────────────────────────────────────────────────────────────────
@@ -13,7 +13,7 @@ DOCKER_FILE=$(echo "${DOCKER_FILE}" | sed -e "s/$ESC_WD\///g")
 CACHE_NAME="${IMAGE_NAME}:cache"
 export DOCKER_BUILDKIT=1
 BUILD="docker"
-if [[ $(docker buildx version 2> /dev/null ) ]]; then
+if [[ $(docker buildx version 2>/dev/null) ]]; then
   builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
   docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
   # ────────────────────────────────────────────────────────────────────────────────
@@ -33,10 +33,11 @@ else
   BUILD+=" --progress=plain"
   BUILD+=" --pull"
 fi
+BUILD+=" --build-arg RUST_VERSION=1.54.0"
 # ────────────────────────────────────────────────────────────────────────────────
 $BUILD $WD
 # ────────────────────────────────────────────────────────────────────────────────
-if [[ $(docker buildx version 2> /dev/null ) ]]; then
+if [[ $(docker buildx version 2>/dev/null) ]]; then
   docker buildx use default
 else
   PUSH="docker push"
--- tools/fd/build.sh
+++ tools/fd/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #-*-mode:sh;indent-tabs-mode:nil;tab-width:2;coding:utf-8-*-
 # vi: tabstop=2 shiftwidth=2 softtabstop=2 expandtab:
-set -euxo pipefail ;
-if [ -z ${IMAGE_NAME+x} ] || [  -z ${IMAGE_NAME} ];then
+set -euxo pipefail
+if [ -z ${IMAGE_NAME+x} ] || [ -z ${IMAGE_NAME} ]; then
   IMAGE_NAME="fjolsvin/$(basename $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))"
 fi
 # ────────────────────────────────────────────────────────────────────────────────
@@ -13,7 +13,7 @@ DOCKER_FILE=$(echo "${DOCKER_FILE}" | sed -e "s/$ESC_WD\///g")
 CACHE_NAME="${IMAGE_NAME}:cache"
 export DOCKER_BUILDKIT=1
 BUILD="docker"
-if [[ $(docker buildx version 2> /dev/null ) ]]; then
+if [[ $(docker buildx version 2>/dev/null) ]]; then
   builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
   docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
   # ────────────────────────────────────────────────────────────────────────────────
@@ -36,7 +36,7 @@ fi
 # ────────────────────────────────────────────────────────────────────────────────
 $BUILD $WD
 # ────────────────────────────────────────────────────────────────────────────────
-if [[ $(docker buildx version 2> /dev/null ) ]]; then
+if [[ $(docker buildx version 2>/dev/null) ]]; then
   docker buildx use default
 else
   PUSH="docker push"
