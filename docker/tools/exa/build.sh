16772e7839ceb0a483fe62766a4c401bb84f1fd6
da-moon <damoon.azarpazhooh@live.com>
2021-08-14 20:10:37 +0000

    refactor(docker): cleaned up duplicate instructions from buildscripts

--- docker/tools/exa/build.sh
+++ docker/tools/exa/build.sh
@@ -6,37 +6,34 @@ if [ -z ${IMAGE_NAME+x} ] || [ -z ${IMAGE_NAME} ]; then
   IMAGE_NAME="fjolsvin/$(basename $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))"
 fi
 # ────────────────────────────────────────────────────────────────────────────────
-
 WD="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 ESC_WD="$(echo "$WD" | sed 's/\//\\\//g')"
 DOCKER_FILE="$(dirname "${BASH_SOURCE[0]}")/Dockerfile"
 DOCKER_FILE=$(echo "${DOCKER_FILE}" | sed -e "s/$ESC_WD\///g")
 CACHE_NAME="${IMAGE_NAME}:cache"
-export DOCKER_BUILDKIT=1
-BUILD="docker"
-if [[ $(docker buildx version 2>/dev/null) ]]; then
-  builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
-  docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
-  # ────────────────────────────────────────────────────────────────────────────────
-  BUILD+=" buildx build"
-  BUILD+=" --file $DOCKER_FILE"
-  BUILD+=" --platform linux/amd64,linux/arm64"
-  BUILD+=" --cache-from type=registry,ref=${CACHE_NAME}"
-  BUILD+=" --cache-to type=registry,mode=max,ref=${CACHE_NAME}"
-  BUILD+=" --tag ${IMAGE_NAME}:latest"
-  BUILD+=" --progress=plain"
-  BUILD+=" --push"
-else
-  BUILD+=" build"
-  BUILD+=" --file $DOCKER_FILE"
-  BUILD+=" --tag ${IMAGE_NAME}:latest"
-  BUILD+=" --cache-from type=registry,ref=${CACHE_NAME}"
-  BUILD+=" --progress=plain"
-  BUILD+=" --pull"
+if [ -z "${DOCKER_BUILDKIT+x}" ] || [ -z "${DOCKER_BUILDKIT}" ]; then
+  export DOCKER_BUILDKIT=1
 fi
-# ────────────────────────────────────────────────────────────────────────────────
+BUILD="docker"
+if [ ! -z "${DOCKER_BUILDKIT+x}" ] && [ "${DOCKER_BUILDKIT}" == "0" ]; then
+  BUILD+=" build"
+else
+  if [[ $(docker buildx version 2>/dev/null) ]]; then
+    builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
+    BUILD+=" buildx build"
+    BUILD+=" --platform linux/amd64,linux/arm64"
+    BUILD+=" --cache-to type=registry,mode=max,ref=${CACHE_NAME}"
+    BUILD+=" --push"
+    docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
+  else
+    BUILD+=" build"
+    BUILD+=" --pull"
+  fi
+  BUILD+=" --cache-from type=registry,ref=${CACHE_NAME}"
+fi
+BUILD+=" --file ${DOCKER_FILE}"
+BUILD+=" --tag ${IMAGE_NAME}:latest"
 $BUILD $WD
-# ────────────────────────────────────────────────────────────────────────────────
 if [[ $(docker buildx version 2>/dev/null) ]]; then
   docker buildx use default
 else
@@ -44,3 +41,4 @@ else
   PUSH+=" ${IMAGE_NAME}:latest"
   $PUSH
 fi
+popd >/dev/null 2>&1
--- docker/tools/ripgrep/build.sh
+++ docker/tools/ripgrep/build.sh
@@ -12,36 +12,3 @@ ESC_WD="$(echo "$WD" | sed 's/\//\\\//g')"
 DOCKER_FILE="$(dirname "${BASH_SOURCE[0]}")/Dockerfile"
 DOCKER_FILE=$(echo "${DOCKER_FILE}" | sed -e "s/$ESC_WD\///g")
 CACHE_NAME="${IMAGE_NAME}:cache"
-export DOCKER_BUILDKIT=1
-BUILD="docker"
-if [[ $(docker buildx version 2> /dev/null ) ]]; then
-  builder="$(echo "$IMAGE_NAME" | cut -d/ -f2)"
-  docker buildx use "${builder}" || docker buildx create --use --name "${builder}"
-  # ────────────────────────────────────────────────────────────────────────────────
-  BUILD+=" buildx build"
-  BUILD+=" --file $DOCKER_FILE"
-#   BUILD+=" --platform linux/amd64,linux/arm64"
-  BUILD+=" --platform linux/amd64"
-  BUILD+=" --cache-from type=registry,ref=${CACHE_NAME}"
-  BUILD+=" --cache-to type=registry,mode=max,ref=${CACHE_NAME}"
-  BUILD+=" --tag ${IMAGE_NAME}:latest"
-  BUILD+=" --progress=plain"
-  BUILD+=" --push"
-else
-  BUILD+=" build"
-  BUILD+=" --file $DOCKER_FILE"
-  BUILD+=" --tag ${IMAGE_NAME}:latest"
-  BUILD+=" --cache-from type=registry,ref=${CACHE_NAME}"
-  BUILD+=" --progress=plain"
-  BUILD+=" --pull"
-fi
-# ────────────────────────────────────────────────────────────────────────────────
-$BUILD $WD
-# ────────────────────────────────────────────────────────────────────────────────
-if [[ $(docker buildx version 2> /dev/null ) ]]; then
-  docker buildx use default
-else
-  PUSH="docker push"
-  PUSH+=" ${IMAGE_NAME}:latest"
-  $PUSH
-fi
